48a147e287aabfc67c7ecf4604127ea425eb7085
luckfer7 <lucasferreira059@gmail.com>
2025-04-16 19:15:44 -0300

    adicionando construtores

---
+++ Programação orientada a objetos/POO/Construtores/Construtores/Construtores/Cake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+    class Cake
+    {
+        public string Sabor;
+        public string Recheio;
+        public string Cobertura;
+        public int Camadas;
+        public string Tamanho;
+        public Cake()
+        {
+            Sabor = "Chocolate";
+            Recheio = "Morango";
+            Cobertura = "Chocolate";
+            Camadas = 2;
+            Tamanho = "Familia";
+        }
+    }
+}
---
+++ Programação orientada a objetos/POO/Construtores/Construtores/Construtores/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Construtores;
+
+Cake cake = new Cake();
+
+Console.WriteLine($"Sabor: {cake.Sabor}");
+Console.WriteLine($"Recheio: {cake.Recheio}");
+Console.WriteLine($"Cobertura de: {cake.Cobertura}");
+Console.WriteLine($"Número de camadas: {cake.Camadas}");
+Console.WriteLine($"Tamanho do bolo: {cake.Tamanho}");
