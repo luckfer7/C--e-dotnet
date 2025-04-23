614d04bb452976d90c20f1b08a09d7667c084758
luckfer7 <lucasferreira059@gmail.com>
2025-04-23 17:06:47 -0300

    adicionando encapsulamento

---
+++ Programação orientada a objetos/POO/Encapsulamento/Encapsulamento/Encapsulamento/ContaBancaria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    public class ContaBancaria
+    {
+        private decimal _saldo; // Campo privado (só acessível dentro da classe)
+
+        // Propriedade pública com controle
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            private set
+            {
+                if (value >= 0) // Validação
+                    _saldo = value;
+            }
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor > 0)
+                Saldo += valor; // Usa a propriedade (não o campo diretamente)
+        }
+    }
+}
---
+++ Programação orientada a objetos/POO/Encapsulamento/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,6 @@
+﻿using Encapsulamento;
+
+ContaBancaria conta = new ContaBancaria();
+conta.Depositar(100);// Válido
+//conta.Saldo(-100); // Erro! O setter é privado
+Console.WriteLine($"Saldo: {conta.Saldo}"); // Só leitura via get
